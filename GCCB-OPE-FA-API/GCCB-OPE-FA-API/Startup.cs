ee19c6f7939080dde04ed9a7735e9339d768d9c7
InfyDaniel <danielvarghese.g@infosys.com>
2024-07-05 14:13:50 +0400

    KeyVault Related changes added and Request Payload changes

--- GCCB-OPE-FA-API/GCCB-OPE-FA-API/Startup.cs
+++ GCCB-OPE-FA-API/GCCB-OPE-FA-API/Startup.cs
@@ -1,8 +1,10 @@
-﻿using AzureFunctions.Extensions.Swashbuckle;
+﻿using Azure.Identity;
+using AzureFunctions.Extensions.Swashbuckle;
 using AzureFunctions.Extensions.Swashbuckle.Settings;
 using GCCB_OPE_FA_API.BLL;
 using GCCB_OPE_FA_API.DataManagers;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
@@ -16,15 +18,22 @@ namespace GCCB_OPE_FA_API
 
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            ConfigurationBuilder _builder = new ConfigurationBuilder();
+            _builder.AddAzureKeyVault(new Uri(Environment.GetEnvironmentVariable("KeyVaultURI")), new DefaultAzureCredential());
+            IConfiguration configuration = _builder.Build();
+
             builder.Services.AddLogging();
             // builder.Services.AddHttpClient();
             builder.Services.AddSingleton(provider =>
             {
                 //var configuration = provider.GetRequiredService<IConfiguration>();
-                var connectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
+                var connectionString = configuration["DatabaseConnectionString"];
                 return new Connection { ConnectionString = connectionString };
             });
             // builder.Services.AddSingleton(proviider => new CacheManager(Environment.GetEnvironmentVariable("RedisConnectionString")));
+
+            Environment.SetEnvironmentVariable("OrderPricingAPIKey", configuration["OrderPricingAPIKey"]);
+            Environment.SetEnvironmentVariable("CatalogueAPIKey", configuration["CatalogueAPIKey"]);
 
             builder.Services.AddScoped<OrderPricing>();
             builder.Services.AddScoped<ConnectionManager>();
